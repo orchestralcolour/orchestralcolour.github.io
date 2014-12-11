678e224a343fe9edbb043c963bcae40bb6fc64a0
protonpopsicle <smeisburger@gmail.com>
2014-12-10 20:54:54 -0500

    added thumbnail generation

---
+++ make_thumbs.fish
@@ -0,0 +1,16 @@
+set loc assets/img/covers
+set size 500
+
+for file in $loc/*.{png,jpg,jpeg}
+    if test -e $loc/thumbs/100x100_(basename $file)
+    else
+       echo 'converting:' $file
+       convert -format png -thumbnail $size -strip $file $loc/thumbs/100x100_(basename $file)
+    end
+
+    if test -e $loc/thumbs/200x200_(basename $file)
+    else
+       echo 'converting:' $file
+       convert -format png -thumbnail $size -strip $file $loc/thumbs/200x200_(basename $file)
+    end
+end
---
+++ pre-commit.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+fish make_thumbs.fish
+git add assets/img/thumbs/
