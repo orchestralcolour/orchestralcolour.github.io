fa5ee67a229c0c83dbfe752aa7da5f996925a32e
protonpopsicle <smeisburger@gmail.com>
2014-12-10 21:19:20 -0500

    updated artwork

--- make_thumbs.fish
+++ make_thumbs.fish
@@ -1,16 +1,15 @@
 set loc assets/img/covers
-set size 500
 
 for file in $loc/*.{png,jpg,jpeg}
     if test -e $loc/thumbs/100x100_(basename $file)
     else
        echo 'converting:' $file
-       convert -format png -thumbnail $size -strip $file $loc/thumbs/100x100_(basename $file)
+       convert -format png -thumbnail 100 -strip $file $loc/thumbs/100x100_(basename $file)
     end
 
     if test -e $loc/thumbs/200x200_(basename $file)
     else
        echo 'converting:' $file
-       convert -format png -thumbnail $size -strip $file $loc/thumbs/200x200_(basename $file)
+       convert -format png -thumbnail 200 -strip $file $loc/thumbs/200x200_(basename $file)
     end
 end
